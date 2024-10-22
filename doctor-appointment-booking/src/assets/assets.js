45cfa524d5babd6cfddb7816b2f58070bf8f1402
choeyeongbi <cyeongb@gmail.com>
2024-10-22 17:06:53 +0900

    초기 commit

---
+++ doctor-appointment-booking/src/assets/assets.js
@@ -0,0 +1,192 @@
+import about from './about.png';
+import add_icon from './add_icon.png';
+import add_doc_icon from './add_doc_icon.png';
+import appointment_doc from './appointment_doc.png';
+import appointment_icon from './appointment_icon.png';
+import appintments_icon from './appintments_icon.svg';
+import blog_icon from './blog_icon.png';
+import cancel_icon from './cancel_icon.svg';
+import contact from './contact.png';
+import dashboard_icon from './dashboard_icon.png';
+import Dermatologist from './dermatologist.svg';
+import doc_main from './doc_main.png';
+import doc1_m from './doc1_m.png';
+import doc1_sm from './doc1_sm.png';
+import doc1 from './doc1.png';
+import doc2 from './doc2.png';
+import doc3 from './doc3.png';
+import doc4 from './doc4.png';
+import doc5 from './doc5.png';
+import doc6 from './doc6.png';
+import doc7 from './doc7.png';
+import doc8 from './doc8.png';
+import doctor_icon from './doctor_icon.svg';
+import doctors_icon from './doctors_icon.png';
+import dropdow_icon from './dropdow_icon.svg';
+import email_icon from './email_icon.png';
+import Gastroenterologist from './Gastroenterologist.svg';
+import General_physician from './General_physician.svg';
+import Gynecologist from './Gynecologist.svg';
+import logo from './logo.svg';
+import Neurologist from './Neurologist.svg';
+import patients_icon from './patients_icon.svg';
+import Pediatricians from './Pediatricians.svg';
+import person1 from './person1.png';
+import person2 from './person2.png';
+import person3 from './person3.png';
+import person4 from './person4.png';
+import profile_img from './profile_img.svg';
+import upload_area from './upload_area.png';
+import uploads_area from './uploads_area.svg';
+
+export const assets={
+    Dermatologist,
+    Gastroenterologist,
+    General_physician,
+    Gynecologist,
+    Neurologist,
+    Pediatricians,
+    about,
+    add_doc_icon,
+    add_icon,
+    appintments_icon,
+    appointment_doc,
+    appointment_icon,
+    blog_icon,
+    cancel_icon,
+    contact,
+    dashboard_icon,
+    doc_main,
+    doc1_m,
+    doc1_sm,
+    doc1,
+    doc2,
+    doc3,
+    doc4,
+    doc5,
+    doc6,
+    doc7,
+    doc8,
+    doctor_icon,
+    doctors_icon,
+    dropdow_icon,
+    email_icon,
+    logo,
+    patients_icon,
+    person1,
+    person2,
+    person3,
+    person4,
+    profile_img,
+    upload_area,
+    uploads_area
+}
+
+export const doctors=[
+    {
+        _id:'doc1',
+        name:'Dr.Richard James',
+        image: doc1,
+        specialty:'General physician',
+        degree:'MBBS',
+        experience:'4 years',
+        fees:50,
+        address:{
+            line1:'27th Cross, Richmond',
+            line2:'Circle, Kingsberry London'
+        }
+    },
+    {
+        _id:'doc2',
+        name:'Dr.Honaldo James',
+        image: doc2,
+        specialty:'General physician',
+        degree:'MBBS',
+        experience:'14 years',
+        fees:80,
+        address:{
+            line1:'27th Cross, Richmond',
+            line2:'Circle, Kingsberry London'
+        }
+    },
+    {
+        _id:'doc3',
+        name:'Dr.Sam Smith',
+        image: doc3,
+        specialty:'General physician',
+        degree:'MBBS',
+        experience:'8 years',
+        fees:60,
+        address:{
+            line1:'27th Cross, Richmond',
+            line2:'Circle, Kingsberry London'
+        }
+    }
+    ,
+    {
+        _id:'doc4',
+        name:'Dr.Sarah Hermione',
+        image: doc4,
+        specialty:'General physician',
+        degree:'MBBS',
+        experience:'11 years',
+        fees:67,
+        address:{
+            line1:'27th Cross, Richmond',
+            line2:'Circle, Kingsberry London'
+        }
+    },
+    {
+        _id:'doc5',
+        name:'Dr.Summer Panels',
+        image: doc5,
+        specialty:'General physician',
+        degree:'MBBS',
+        experience:'6 years',
+        fees:53,
+        address:{
+            line1:'27th Cross, Richmond',
+            line2:'Circle, Kingsberry London'
+        }
+    },
+    {
+        _id:'doc6',
+        name:'Dr.Chifield Sanonne',
+        image: doc6,
+        specialty:'General physician',
+        degree:'MBBS',
+        experience:'15 years',
+        fees:90,
+        address:{
+            line1:'27th Cross, Richmond',
+            line2:'Circle, Kingsberry London'
+        }
+    },
+    {
+        _id:'doc7',
+        name:'Dr.Soleman Pubson',
+        image: doc7,
+        specialty:'General physician',
+        degree:'MBBS',
+        experience:'21 years',
+        fees:105,
+        address:{
+            line1:'27th Cross, Richmond',
+            line2:'Circle, Kingsberry London'
+        }
+    },
+    {
+        _id:'doc8',
+        name:'Dr.Snoop Dog',
+        image: doc8,
+        specialty:'General physician',
+        degree:'MBBS',
+        experience:'1 years',
+        fees:10,
+        address:{
+            line1:'27th Cross, Richmond',
+            line2:'Circle, Kingsberry London'
+        }
+    }
+    // 17분,30까지
+]
