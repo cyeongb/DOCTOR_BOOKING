36b02fdde0908342ff338b721e6729d5bdf184f7
= <=>
2024-10-26 00:45:35 +0900

    profile token 여부에 따라 변경 로직 추가.

--- doctor-appointment-booking/src/components/Navbar.jsx
+++ doctor-appointment-booking/src/components/Navbar.jsx
@@ -1,37 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
-import {NavLink, useNavigate} from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
 
-const navigate= useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
 
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400 ">
-      <img className="w-44 cursor-pointer" src={assets.logo} alt=""/> 
+      <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
       <ul className="hidden md:flex items-start gap-5 font-medium ">
-        <NavLink to='/'>
+        <NavLink to="/">
           <li className="py-1">HOME</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        <NavLink to='/doctors'>
+        <NavLink to="/doctors">
           <li className="py-1">ALL DOCTORS</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        <NavLink to='/about'>
+        <NavLink to="/about">
           <li className="py-1">ABOUT</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        <NavLink to='/contact'>
+        <NavLink to="/contact">
           <li className="py-1">CONTACT</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
       <div className="flex items-center gap-4">
-        <button onClick={()=>navigate('/login')} className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block ">Create account</button>
+        {token ? (
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <img src={assets.dropdow_icon} alt="" />
+          </div>
+        ) : (
+          <button
+            onClick={() => navigate("/login")}
+            className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block "
+          >
+            Create account
+          </button>
+        )}
       </div>
     </div>
-    //42.07초
+    //45:50
   );
 };
 
