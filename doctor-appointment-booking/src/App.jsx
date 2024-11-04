7e1555f945fdc1276f5a29823c1fdff1a878ff0c
choeyeongbi <cyeongb@gmail.com>
2024-11-05 02:22:24 +0900

    footer 생성 및 추가

--- doctor-appointment-booking/src/App.jsx
+++ doctor-appointment-booking/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from "./pages/MyProfile";
 import MyAppointments from "./pages/MyAppointments";
 import Appointment from "./pages/Appointment";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
 
@@ -26,6 +27,7 @@ const App = () => {
       <Route path="/myProfile" element={<MyProfile />}/>
       <Route path="/myAppointments" element={<MyAppointments />}/>
      </Routes>
+     <Footer />
     </div>
   );
 };
