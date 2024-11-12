40ae5518aaa28fdcfc360eb9fc67610fc0610cec
choeyeongbi <cyeongb@gmail.com>
2024-11-13 01:07:01 +0900

    예약 기능 수정

--- doctor-appointment-booking/src/pages/Appointment.jsx
+++ doctor-appointment-booking/src/pages/Appointment.jsx
@@ -7,10 +7,11 @@ import { assets } from "../assets/assets";
 const Appointment = () => {
   const { docId } = useParams();
   const { doctors, currencySymbol } = useContext(AppContext);
+  const daysOfWeek = ['SUN','MON','TUE','WED','THU','FRI', 'SAT'];
 
   const [docInfo, setDocInfo] = useState(null); //해당 의사 정보
 
-  // booking slot
+  // 예약시스템
   const [docSlot, setDocSlot] = useState([]);
   const [slotIndex, setSlotIndex] = useState(0);
   const [slotTime, setSlotTime] = useState("");
@@ -27,21 +28,21 @@ const Appointment = () => {
     let today = new Date();
 
     for (let i = 0; i < 7; i++) {
-      //날짜를 오늘~일주일 뒤, 시간은 10시~21시 까지 설정 가능.
+      //예약날짜를 오늘~일주일 뒤, 시간은 10시~21시 전 까지 설정 가능.
       let currentDate = new Date(today);
       currentDate.setDate(today.getDate() + i);
       console.log("currentDate:::" + currentDate);
 
-      let endTime = new Date(); //운영 마감 시간
+      let endTime = new Date(); //운영 마감 시간 21:00
       endTime.setDate(today.getDate() + i);
       endTime.setHours(21, 0, 0, 0);
       console.log("endTime:::" + endTime);
 
-      if (today.getDate() === currentDate.getDate()) { //오늘이면
+      if (today.getDate() === currentDate.getDate()) {
         currentDate.setHours(
           currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10
         );
-        currentDate.setMinutes(currentDate.getMinutes()>30? 30:0);
+        currentDate.setMinutes(currentDate.getMinutes()>30? 30:0); //분을 30분 단위로 설정하기 위함.
       }else{
         currentDate.setHours(10);
         currentDate.setMinutes(0);
@@ -49,9 +50,9 @@ const Appointment = () => {
       
       let timeSlots = [];
       
-      while(currentDate < endTime){ //current date 가 마감시간보다 전 이면
-        let formatTime = currentDate.toLocaleTimeString([],{hour:'2-digit',minute:'2-digit'})
-        
+      while(currentDate < endTime){ //currentDate 가 마감시간보다 전 이면
+        let formatTime = currentDate.toTimeString([],{hour:'2-digit',minute:'2-digit'})
+        console.log("formatTime::"+formatTime)
         //slot을 배열에 넣음
         timeSlots.push({
           datetime : new Date(currentDate),
@@ -75,7 +76,7 @@ const Appointment = () => {
 
 
   useEffect(() => {
-    console.log("docSlot:::"+docSlot)
+    console.log(docSlot)
   }, [docSlot]);
 
   return (
@@ -123,6 +124,29 @@ const Appointment = () => {
               </span>
               <span>(per Hour)</span>
             </p>
+          </div>
+        </div>
+
+           {/* 2.48.11,, */}
+        {/* Booking slot  */}
+        <div className="sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700">
+          <p>Booking Slots</p> <p className=" flex font-light text-sm">scroll the time!</p>
+          <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
+            {
+              docSlot.length && docSlot.map((item,i)=>(
+                <div onClick={()=>setSlotIndex(i)} className={`text-center py-6 min-w-16 rounded-2xl cursor-pointer ${slotIndex === i ? 'bg-primary text-white' : 'border border-gray-200' }`} key={i}>
+                  <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                  <p>{item[0] && item[0].datetime.getDate()}</p>
+                </div>
+              ))
+            }
+          </div>
+          <div className="flex items-center gap-3 w-full overflow-x-scroll mt-4">
+            {docSlot.length && docSlot[slotIndex].map((item,i)=>(
+              <p onClick={()=>setSlotTime(item.time)} key={i} className={`text-sm font-light flex-shrink-0 px-4 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`}>
+              {item.time.substr(0,5)}
+              </p>
+            ))}
           </div>
         </div>
       </div>
